8170c0c85e5fbd668ad2f626d190291ab47e1067
sebossert@students.zhaw.ch <sebossert@protonmail.com>
2020-10-11 03:52:54 +0200

    localstorage

--- src/ts/highscore.ts
+++ src/ts/highscore.ts
@@ -4,40 +4,61 @@ import { Player } from "./player";
  * @class High
  */
 export class Highscore {
-  entries: Array<Player> = [];
+  entries: Array<{ name: string; points: number }> = [];
   element: HTMLOListElement;
+  numberOfEntries: number = 10;
   /**
    *
    * @param element
    */
   constructor(element: HTMLOListElement) {
     this.element = element;
+    this.load();
+    this.update(false);
   }
   add(player: Player) {
     let added = false;
     for (let i = 0; i < this.entries.length; i++) {
       const entry = this.entries[i];
-      if (entry.points.current < player.points.current) {
-        this.entries.splice(i, 0, player);
+      if (entry.points < player.points.current) {
+        this.entries.splice(i, 0, {
+          name: player.name,
+          points: player.points.current,
+        });
         added = true;
         break;
       }
     }
     if (this.entries.length < this.element.childNodes.length && !added) {
       console.log("player bad");
-      this.entries.push(player);
+      this.entries.push({
+        name: player.name,
+        points: player.points.current,
+      });
     }
     this.update();
   }
-  update() {
-    for (let i = 0; i < this.entries.length; i++) {
-      const childNodeIndex = i * 2 + 1; //TODO
-      this.element.childNodes[childNodeIndex].textContent = this.createEntryText(
-        this.entries[i]
-      );
+  load() {
+    for (let i = 0; i < this.numberOfEntries; i++) {
+      const stored = localStorage.getItem("entry-" + i);
+      if (stored) {
+        const entry = JSON.parse(stored);
+        this.entries[i] = entry;
+      }
     }
   }
-  createEntryText(entry: Player) {
-    return entry.name + " : " + entry.points.current;
+  update(updateEntries: boolean = true) {
+    for (let i = 0; i < this.entries.length; i++) {
+      if (updateEntries) {
+        localStorage.setItem("entry-" + i, JSON.stringify(this.entries[i]));
+      }
+      const childNodeIndex = i * 2 + 1;
+      this.element.childNodes[
+        childNodeIndex
+      ].textContent = this.createEntryText(this.entries[i]);
+    }
+  }
+  createEntryText(entry: { name: string; points: number }) {
+    return entry.name + " : " + entry.points;
   }
 }
